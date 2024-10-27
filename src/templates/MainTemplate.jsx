d00075f52f9b8b4733555d0c65cde2b532cd0655
Hubert Majewski <130083088+matthiasmajor@users.noreply.github.com>
2024-10-27 21:51:50 +0100

    new new

--- src/templates/MainTemplate.jsx
+++ src/templates/MainTemplate.jsx
@@ -2,8 +2,8 @@ import * as React from "react";
 import { theme } from "../assets/theme";
 import { GlobalStyle } from "../assets/GlobalStyles";
 import { ThemeProvider } from "styled-components";
-import { Navigation } from "./Navigation/Navigation";
-import { Footer } from "./Footer/Footer";
+import { Navigation } from "../components/Navigation/Navigation";
+import { Footer } from "../components/Footer/Footer";
 
 export const MainTemplate = ({ children }) => {
   return (
